d5ca07aefadd3ae1f14a414df48d1740bf98c06a
jordan <jordjamestaylor@gmail.com>
2022-07-27 09:59:58 -0400

    commit tree 7d7810726380c7ea6df4eb711a61f7183a8a707c

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,35 @@
 class Movie < ActiveRecord::Base
 
+    def self.create_with_title newTitle
+        self.create(title: newTitle)
+    end
+
+    def self.first_movie
+        self.first
+    end
+
+    def self.last_movie
+        self.last
+    end
+
+    def self.movie_count
+        self.count
+    end
+
+    def self.find_movie_with_id id
+        self.find(id)
+    end
+
+    def self.find_movie_with_attributes att
+        self.find_by(att)
+    end
+
+    def self.find_movies_after_2002
+        self.where('release_date > 2002')
+    end
+
+    def update_with_attributes val
+        assign_attributes(val)
+        save
+    end
 end
